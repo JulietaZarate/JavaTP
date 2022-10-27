e7d3607b14dbb91be0afbeb059a8568a8c4447e2
JulietaZarate <julizarate9965@gmail.com>
2022-10-27 12:33:00 -0300

    actualización 27/10

--- src/main/java/ModelDao/LocalidadesDao.java
+++ src/main/java/ModelDao/LocalidadesDao.java
@@ -11,14 +11,16 @@ import model.Localidad;
 public class LocalidadesDao  {
 	
 
-	public LinkedList<Localidad> buscar()
+	public LinkedList<Localidad> getLocalidades()
 	{
+		LinkedList<Localidad> localidades = new LinkedList<>();
 		PreparedStatement st=null;
 		ResultSet rs=null;
-		LinkedList<Localidad> localidades = new LinkedList<>();	
 		try {
 			st= DbConnector.getInstancia().getConn().prepareStatement("Select * FROM localidades");
+			//---Se establece y se crea la sentencia SQL--
 			rs= st.executeQuery();
+			//--Se ejecuta la sentencia SQL--
 			{
 				while(rs.next()) 
 				{
@@ -27,6 +29,7 @@ public class LocalidadesDao  {
 					loc.setLocalidad(rs.getString("localidad"));
 					localidades.add(loc);
 				}
+				//Se recorre el ResultSet
 			}
 		} 
 		catch (SQLException e) 
@@ -37,4 +40,38 @@ public class LocalidadesDao  {
 		return localidades;
 
 	}
+	
+	
+	public void add(Localidad l) {
+		PreparedStatement stmt= null;
+		ResultSet keyResultSet=null;
+		try {
+			stmt=DbConnector.getInstancia().getConn().
+					prepareStatement(
+							"insert into localidades(id, id_privincia, localidad) values(?,?)",
+							PreparedStatement.RETURN_GENERATED_KEYS
+							);
+			stmt.setInt(1, l.getId_privincia());
+		    stmt.setString(2, l.getLocalidad());
+			stmt.executeUpdate();
+			
+			keyResultSet=stmt.getGeneratedKeys();
+            if(keyResultSet!=null && keyResultSet.next()){
+                l.setId(keyResultSet.getInt(1));
+            }
+
+			
+		}  catch (SQLException e) {
+            e.printStackTrace();
+		} finally {
+            try {
+                if(keyResultSet!=null)keyResultSet.close();
+                if(stmt!=null)stmt.close();
+                DbConnector.getInstancia().releaseConn();
+            } catch (SQLException e) {
+            	e.printStackTrace();
+            }
+	
+		}
+  }
 }
